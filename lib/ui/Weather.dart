c51ffb8b8e81a234c1d2f70f376507e695d7c752
adnrbp <adnrbp@users.noreply.github.com>
2019-12-08 21:50:35 -0500

    [view] Weather Widget: add dateSection

--- lib/ui/Weather.dart
+++ lib/ui/Weather.dart
@@ -4,12 +4,23 @@ import 'package:intl/intl.dart';
 class Weather extends StatelessWidget {
   @override
   Widget build(BuildContext context){
+    Widget dateSection = Container(
+      child: Text(
+        DateFormat('MMMM d, H:m').format(DateTime.now()),
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 24.0,
+        ),
+      ),
+    );
+
     return Container(
       padding: const EdgeInsets.all(60.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start, //align to left hand side of the column
         children: <Widget>[
-
+          dateSection,
         ],
       ),
     );
